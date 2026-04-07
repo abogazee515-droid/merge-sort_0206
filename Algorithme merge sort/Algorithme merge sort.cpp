5e26c3badafe1814d00887a23b1ffb82ac71f525
abogazee515-droid <abogazee515@gmail.com>
2026-04-07 14:30:47 +0700

    start input function and user prompt

--- Algorithme merge sort/Algorithme merge sort.cpp
+++ Algorithme merge sort/Algorithme merge sort.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 // create main array & temporary array
 int arr[20], B[20];
+
 // n is array input size
 int n;
 
@@ -12,96 +13,3 @@ void input()
     {
         cout << "Masukkan Panjang element array : ";
         cin >> n;
-
-        if (n <= 20)
-        {
-            break;
-        }
-        else
-        {
-            cout << "\nMaksimal panjang array adalah 20";
-        }
-    }
-
-    cout << "\n--------------------------" << endl;
-    cout << "\nInputkan Isi element array" << endl;
-    cout << "\n--------------------------" << endl;
-
-    for (int i = 0; i < n; i++)
-    {
-        cout << "Array index ke-" << i << " : ";
-        cin >> arr[i];
-    }
-}
-
-// create function mergeSort
-void mergeSort(int low, int high)
-{
-    if (low >= high) // step 1
-    {
-        return; // step 1.a
-    }
-
-    int mid = (low + high) / 2; // step 2
-
-    // step 3 (recursion)
-    mergeSort(low, mid);       // step 3.a
-    mergeSort(mid + 1, high);  // step 3.b
-
-    // step 4 (merge)
-    int i = low;
-    int j = mid + 1;
-    int k = low;
-
-    while (i <= mid && j <= high)
-    {
-        if (arr[i] <= arr[j])
-        {
-            B[k] = arr[i];
-            i++;
-        }
-        else
-        {
-            B[k] = arr[j];
-            j++;
-        }
-        k++;
-    }
-
-    while (j <= high)
-    {
-        B[k] = arr[j];
-        j++;
-        k++;
-    }
-
-    while (i <= mid)
-    {
-        B[k] = arr[i];
-        i++;
-        k++;
-    }
-
-    // copy back
-    for (int x = low; x <= high; x++)
-    {
-        arr[x] = B[x];
-    }
-}
-
-void output()
-{
-    cout << "\nData setelah diurutkan (Merge Sort): ";
-    for (int i = 0; i < n; i++)
-    {
-        cout << arr[i] << " ";
-    }
-    cout << endl;
-}
-
-int main()
-{
-    input();
-    mergeSort(0, n - 1);
-    output();
-}
